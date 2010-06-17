2850c422f35e2860c021eb2cfc5b0e59c693088f
Vlad <vlad.shcherbina@gmail.com>
2010-06-17 14:16:37 -0700

    add test project

---
+++ test_proj/src/main.py
@@ -0,0 +1,15 @@
+try:
+    import psyco
+    psyco.full()
+except ImportError:
+    print "you do not have psyco, but (hopefully) it's fine"
+
+import numpy
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
