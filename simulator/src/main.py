bcad752d57827679bb49b420c808c5ae5bc7d90d
Vlad <vlad.shcherbina@gmail.com>
2010-06-18 09:57:44 -0700

    minor changes

--- simulator/src/main.py
+++ simulator/src/main.py
@@ -10,16 +10,24 @@ Chamber = namedtuple('Chamber','upper lower')
 
 
 class Car(object):
-    __slots__ = [
-        'num_tanks',
-        'main_chambers',
-        'aux_chambers',
-        ]
     # ***_chamers is list of Chambers
     # each chamber is named tuple of two lists (pipes)
     # each pipe is list of integer (tank numbers starting from 0)
     
     # fuels is list of numpy 2d arrays (preferably with dtype=int)
+    
+    __slots__ = [
+        'num_tanks',
+        'main_chambers',
+        'aux_chambers',
+        ]
+
+    @staticmethod
+    def from_stream(self, s):
+        raise NotImplementedError()
+    
+    def to_stream(self):
+        raise NotImplementedError()
 
     def __init__(self, num_tanks):
         self.num_tanks = num_tanks
@@ -32,26 +40,23 @@ class Car(object):
         for chamber in self.aux_chambers:
             yield chamber, False
 
-    def testOnInput(self, fuels, input):
+    def test_on_input(self, fuels, input):
         n = len(input)
-        for chamber in self.main_chambers:
-            if not testChamberOnInput(chamber, fuels, input, main=True):
-                return False
-        for chamber in self.aux_chambers:
-            if not testChamberOnInput(chamber, fuels, input, main=False):
+        for chamber, isMain in self.all_chambers():
+            if not test_chamber_on_input(chamber, fuels, input, main=isMain):
                 return False
         return True
 
-    def testOnFuel(self, fuels, num_tests=100):
+    def test_on_fuel(self, fuels, num_tests=100):
         assert len(fuels) == self.num_tanks
         
         n = fuels[0].shape[0]
         assert all(f.shape == (n, n) for f in fuels)
 
         for i in range(num_tests):
-            input = array([randrange(10) for i in range(n)], dtype=int)
+            input = [randrange(10) for i in range(n)]
             input[0] += 1
-            if not self.testOnInput(fuels, input):
+            if not self.test_on_input(fuels, input):
                 return False
         return True
     
@@ -72,37 +77,38 @@ class Car(object):
                 coeffs[i] -= 1
             # i'm not doing it as difference of lpSums because it's buggy
             term = lpSum(coeff*var for coeff, var in zip(coeffs, vars))
+            assert len(term) != 0, 'pipes are equivalent'
             if isMain:
                 problem += term >= eps
             else:
                 problem += term >= 0
         
-        #print problem
-        
         result = problem.solve(GLPK(msg=False))
         
-        #print LpStatus[result]
-        #for v in vars:
-        #    print v,'=', value(v)
-            
-        assert result == LpStatusOptimal
+        if result != LpStatusOptimal:
+            print problem
+            result = problem.solve(GLPK(msg=True))
+            print LpStatus[result]
+            assert False
         
         # go back from logspace to linear space
         return [2**value(v) for v in vars]
         
         
 
-def pipeFunction(pipe, fuels, input):
-    x = input
+def pipe_function(pipe, fuels, input):
+    if isinstance(input, list):
+        input = array(input, dtype=int)
+        
     for section in pipe:
-        x = dot(fuels[section], x)
-    return x
+        input = dot(fuels[section], input)
+    return input
 
 
-def testChamberOnInput(chamber, fuels, input, main):
-    n, = input.shape
-    upper = pipeFunction(chamber.upper, fuels, input)
-    lower = pipeFunction(chamber.lower, fuels, input)
+def test_chamber_on_input(chamber, fuels, input, main):
+    n = len(input)
+    upper = pipe_function(chamber.upper, fuels, input)
+    lower = pipe_function(chamber.lower, fuels, input)
     
     return all(up >= lo for up, lo in zip(upper, lower)) and \
         (not main or upper[0] > lower[0])
@@ -111,15 +117,15 @@ def testChamberOnInput(chamber, fuels, input, main):
 
 def main():
     car = Car(2)
-    car.main_chambers.append(Chamber([1, 0, 0, 1], [0, 1, 0]))
+    car.main_chambers.append(Chamber([1, 0, 0], [1, 1, 0]))
     
-    #fuels = [array([[1]]), array([[2]])]
-    #print car.testOnFuel(fuels)
+    #fuels = [array([[1]]), array([[1]])]
+    #print car.test_on_fuel(fuels)
     
     fuels = car.solveLP()
     print fuels
     fuels = [array([[f]]) for f in fuels]
-    print car.testOnFuel(fuels)
+    print car.test_on_fuel(fuels)
 
 
 if __name__ == '__main__':
