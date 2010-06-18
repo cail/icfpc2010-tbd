a3c311f5816605ffeeba8baae8a67afafa6469b9
Vlad <vlad.shcherbina@gmail.com>
2010-06-18 14:00:03 -0700

    eval for scheme

--- simulator/src/scheme.py
+++ simulator/src/scheme.py
@@ -2,10 +2,24 @@ import re
 
 special_contact = 'X'
 
+
 def parse_contact(e):
     assert e != special_contact
     m = re.match("(\d)+(L|R)", e)
     return (int(m.group(1)), m.group(2))
+
+
+def gate_function(fn, left, right):
+    table =\
+    [
+       [[(0,2),(1,2),(2,2)],
+        [(1,0),(2,0),(2,0)],
+        [(2,0),(1,0),(0,0)]],
+    ] 
+    #return table[fn][left][right]
+    
+    return (left+right)%3, 1 
+
 
 class Scheme(object):
     __slots__ = [
@@ -49,7 +63,26 @@ class Scheme(object):
         result.append(self.from_[special_contact])
         
         return '\n'.join(result)
-
+    
+    def eval(self, inputs):
+        result = []
+        
+        values = {}
+        for i in range(self.num_nodes):
+            values[str(i)+'L'] = 0
+            values[str(i)+'R'] = 0
+            
+        for step, input in enumerate(inputs):
+            values[special_contact] = input
+            for i in range(self.num_nodes):
+                values[str(i)+'L'], values[str(i)+'R'] =\
+                    gate_function(self.function[i],
+                                  values[self.from_[str(i)+'L']],
+                                  values[self.from_[str(i)+'R']])
+            result.append(values[self.from_[special_contact]])
+        return result
+        
+        
 if __name__ == '__main__':
     sch = Scheme()
     sch.add_node(0,0)
@@ -61,3 +94,8 @@ if __name__ == '__main__':
     sch.connect('1R','1L')
     
     print sch
+    
+    inputs = [i%3 for i in range(17)]
+    outputs = sch.eval(inputs)
+    print 'in ',''.join(map(str, inputs))
+    print 'out',''.join(map(str, outputs))
