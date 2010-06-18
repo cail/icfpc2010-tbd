0506f52d57476a27bf7eaa27f2b8b764a03c30db
Vlad <vlad.shcherbina@gmail.com>
2010-06-18 06:21:12 -0700

    pulp install instructions

--- test_proj/src/main.py
+++ test_proj/src/main.py
@@ -5,11 +5,23 @@ except ImportError:
     print "you do not have psyco, but (hopefully) it's fine"
 
 import numpy
+from pulp import *
 
 
 def main():
-    pass
-
+    x = LpVariable("x")
+    y = LpVariable("y")
+    
+    problem = LpProblem("test problem", LpMinimize)
+    problem += x <= 2
+    problem += x+y >= 1
+    
+    result = problem.solve(GLPK(msg=False))
+    
+    print LpStatus[result]
+    
+    print 'x =',value(x)
+    print 'y =',value(y)
 
 if __name__ == '__main__':
     main()
