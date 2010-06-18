51c4ebe7be5633a8723779854230ca03477ce1a1
Igor Russkih <irusskih@gmail.com>
2010-06-18 23:21:08 +0400

    improving graph layout abit

--- circuit_viz/src/circuit_viz.py
+++ circuit_viz/src/circuit_viz.py
@@ -20,6 +20,7 @@ def main():
     nodes_count = int( lines[0].strip()[0:-2] ) + 2
     
     print "digraph unix {"
+    print ' rank=sink; weight=1;  '
     for node_no in range(0, int(nodes_count)):
         
         line = lines[node_no].strip()
@@ -53,8 +54,8 @@ def main():
         #print '"{0}":{2} -> "{1}":L;'.format(win_left[:-1],  node_no, win_left[-1:])
         #print '"{0}":{2} -> "{1}":R;'.format(win_right[:-1], node_no, win_right[-1:])
 
-        print '"{0}":L -> {1};'.format(node_no, print_target(node_out_left))
-        print '"{0}":R -> {1};'.format(node_no, print_target(node_out_right))
+        print '"{0}":L:s -> {1}:n;'.format(node_no, print_target(node_out_left))
+        print '"{0}":R:s -> {1}:n;'.format(node_no, print_target(node_out_right))
 
         print " "
         
