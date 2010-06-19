e5dbfc9f1e3668e7e17933c71cb9ec6bab140902
Igor Russkih <irusskih@gmail.com>
2010-06-19 16:01:50 +0400

    submit_fuel now works

--- simulator/src/submit_fuel.py
+++ simulator/src/submit_fuel.py
@@ -17,34 +17,31 @@ def login():
     br.submit()
     return br
 
-def submit_fuel(vehicle, fuel):
+def submit_fuel(car, fuel):
 
     br = login()
     
-    response = br.open("http://icfpcontest.org/icfp10/instance/{0}/solve/form".format(vehicle))
+    res = br.open("http://icfpcontest.org/icfp10/instance/{0}/solve/form".format(car))
     
     br.select_form(nr=0)
         
-    br["problem"] = vehicle
-    br["exampleSolution.contents"] = fuel
-    
+    br["contents"] = fuel
+
     response = br.submit()
     
-    #print response.info()  # headers
-    body =  response.read()  # body
+    body =  response.read()
 #    print body
     
     re_spanerr = re.compile(r"class=\"errors\"\>(.*?)\<\/span\>", re.S+re.M)
-    re_err = re.compile(r"\<pre\>(.*?)\</pre\>", re.S+re.M)
+    re_pre = re.compile(r"\<pre\>(.*?)\</pre\>", re.S+re.M)
 
-    m = re_err.search(body)
+    mpre = re_pre.search(body)
     mspan = re_spanerr.search(body)
     if mspan:
         error = mspan.group(1)
         return error
-    elif m:
-        error = m.group(1)
-        return error
+    elif mpre:
+        return mpre.group(1)
     else:
         return "OK " + body
 
