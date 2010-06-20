949f1945978803072ae7ddda99aee986f92b3951
Igor Russkih <irusskih@gmail.com>
2010-06-20 20:18:58 +0400

    fetch stats with ranking table updated

--- simulator/src/fetch_stats.py
+++ simulator/src/fetch_stats.py
@@ -24,10 +24,47 @@ def fetch_stats(br=None):
     
     return (score, csolved, csubmitted)
     
+def fetch_results():
+    br = mechanize.Browser()
+    response = br.open("http://icfpcontest.org/icfp10/score/teamAll")
+    
+    body = response.read()
+    #body = open('body').read()
+    #print body
+    bs = BeautifulSoup(body)
+
+    tbody = bs.fetch('table')
+    
+    if len(tbody) < 1:
+        print "Server err"
+        return None
+    
+    tds = tbody[0].fetch('td')
+
+    results = []
+    for i in range(0, len(tds)/2):
+        if i < 2:
+            continue
+        val = tds[i*2].renderContents()
+        name = tds[i*2+1].renderContents()
+        if val == '0,000':
+            continue
+        if name == 'TBD':
+            rank = i
+        results.append(  (name, val) )
+
+    return (rank, results)
+    
+    
 if __name__ == '__main__':
     
-    (score, csolved, csubmitted) = fetch_stats()
+    rank, results = fetch_results()
+    print "Rank: ", rank
     
+    (score, csolved, csubmitted) = fetch_stats()
     print "Score: ", score
     print "Solved: ", csolved
     print "Submitted: ", csubmitted
+    
+    for r in results:
+        print r[0], " - ", r[1]
