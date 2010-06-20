ca1dcd306aaa28dee8e03a12f244d3415ddc652d
Fj <fj.mail@gmail.com>
2010-06-21 01:43:12 +0300

    factory_builder in progress

---
+++ fj/src/factory_builder.py
@@ -0,0 +1,61 @@
+from scheme import Scheme
+
+def build_sequences(output):
+    node_transitions = ('00', '02', '12', '21', '11')
+    period = len(node_transitions)
+    seq = []
+    current_offset = 0
+    for bit in output:
+        for length in range(1, period + 1):
+            current_offset = (current_offset + 1) % period
+            if bit == node_transitions[current_offset][0]:
+                break
+        else: assert False, 'WTF? ' + str(output) 
+        seq.append(length)
+    return seq
+
+def build_scheme(sequences):
+    s = Scheme()
+    total_cnt = sum(sequences)
+    input_node = prev_start = total_cnt - sequences[-1]
+    current = 0
+    for length in sequences:
+        last = current + length - 1
+        for current in range(current, last):
+            s.connect(str(current) + 'L', str(current + 1) + 'L')
+            s.connect(str(current) + 'R', str(current + 1) + 'R')
+        current = last
+        s.connect(str(current) + 'L', str(prev_start) + 'L')
+        s.connect(str(current) + 'R', str(prev_start) + 'R')
+        current = last + 1
+        prev_start = current - length
+    s.connect(str(sequences[0] - 1) + 'L', 'X')
+    s.connect('X', str(input_node) + 'L')
+    print s.to
+    print s.from_
+    
+    return s
+    
+seq = build_sequences('0')
+print seq
+s = build_scheme(seq)
+print s
+    
+        
+
+#sch = Scheme()
+#sch.add_node(0,0)
+#sch.connect('X','0L')
+#    #sch.connect('0L','X')
+#    #sch.connect('0R','0R')
+#    
+#
+#    sch = Scheme.load(open("../data/sample_scheme.txt"))
+#    inputs = [0,2,2,2,2,2,2,0,2,1,0,1,1,0,0,1,1] # from problem description
+#
+#    print sch
+#    
+#    outputs = list(sch.eval(inputs))
+#    assert outputs == key
+#    print 'in ',''.join(map(str, inputs))
+#    print 'out',''.join(map(str, outputs))
