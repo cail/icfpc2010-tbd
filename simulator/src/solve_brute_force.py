458d9845cdb20fdced8e9e5a8bfb6c2eeb9bca1e
Vlad <vlad.shcherbina@gmail.com>
2010-06-20 03:32:07 -0700

    improve fuel brute force boundaries

--- simulator/src/solve_brute_force.py
+++ simulator/src/solve_brute_force.py
@@ -1,8 +1,14 @@
+from math import *
 from itertools import *
 
-def solve_brute_force(car, limit=3):
+from car import fuel_to_stream
+
+def solve_brute_force(car):
+    n = 20000
+    limit = int(0.5 + exp(log(n)/car.num_tanks))
+    
     fuels = product(*[range(1,limit)]*car.num_tanks)
-    fuels = sorted(fuels, key=sum)
+    fuels = sorted(fuels, key=lambda fuel: len(fuel_to_stream(list(fuel))))
     
     for fuel in fuels:
         if car.test_on_fuel(fuel):
